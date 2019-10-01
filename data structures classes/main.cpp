60c0a6dd20edb3f26f53cd362dac3c120d3840f8
Andrei Zhuravlev <dwasdx@gmail.com>
2019-10-01 11:53:31 +0300

    Initial Commit

---
+++ data structures classes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  data structures classes
+//
+//  Created by Андрей Журавлев on 01/10/2019.
+//  Copyright © 2019 Андрей Журавлев. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
